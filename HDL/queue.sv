70038412ec555d665a45cc9bec40b43620fa8ed1
Bernardo F. Fraga <bfoquesdefraga@gmail.com>
2025-06-02 23:52:37 +0000

    added and implemented the states IDLE, ENQ and DEQ

--- HDL/queue.sv
+++ HDL/queue.sv
@@ -27,10 +27,23 @@ module queue(
     input logic deq_in,
 
     output logic data_out,
-    output logic len_out
+    output logic [7:0] len_out // sinal de 8 bits
 );
 
-typedef enum logic [1:0] {ENQ, DEQ}
+typedef enum logic [1:0] {
+    IDLE,
+    ENQ,
+    DEQ
+    }state_t;
+
+    state_t state;
+
+    full = (len_out == 8);
+    empty = (len_out == 0);
+
+   logic [7:0] queue [0:7];
+   logic [2:0] head, tail;
+
 
 
 always @(posedge clock or posedge reset) begin
@@ -41,13 +54,35 @@ always @(posedge clock or posedge reset) begin
     data_out    <= 0;
     len_out     <= 0;
     end else begin
-           
-            
+        case(state) 
 
+               
+            IDLE: begin  
+                $display("Entrei no estado IDLE");
+               if (enq_in && !full) // Se o sinal enq_in estiver alto e a fila não estiver cheia vai para o estado enqueue
+                   state <= ENQ; 
+                else if (deq_in && !empty) // Se o sinal deq_in estiver alto e a fila estiver vazia vai apra o estado dequeue
+                    state <= DEQ;
+                else
+                    state <= IDLE; // Se não estiver alto nenhum dos dois sinais repete IDLE
+            end
 
+            ENQ: begin
+                $display("Entrei no estado ENQ");
+                queue[tail] <= data_in; // insere dado na posição aonde tail indica
+                tail <= (tail + 1) % 8;    // incrementa circularmente
+                len_out <= len_out + 1;
+                state <= IDLE;
+            end  
 
+            DEQ: begin 
+                $display("Entrei no estado DEQ");
+                data_out <= queue[head]; // retira dado da posição aonde head indica
+                head <= (head + 1) % 8;    // incrementa circularmente
+                len_out <= len_out - 1;
+                state <= IDLE;
+            end
 
-end
-
+    end
 
 endmodule
